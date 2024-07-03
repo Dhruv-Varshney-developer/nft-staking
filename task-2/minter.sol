3cdb45c7c0b50c89ee7f7c0fe77b1a678a6fec98
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2024-07-04 04:37:57 +0530

    [Fixed]:minter.sol

--- task-2/minter.sol
+++ task-2/minter.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "module-2/myERC721.sol";
+//import "./myERC721.sol";
+import "module-2/task-2/myERC721.sol";
 
-contract minter is Ownable {
+contract minter is Ownable(msg.sender) {
     IERC20 public paymentToken;
     MyNFT public nftContract;
     uint256 public constant PRICE = 10 * 10 ** 18; // 10 tokens with 18 decimals
+
 
     constructor(IERC20 _paymentToken, MyNFT _nftContract) {
         paymentToken = _paymentToken;
@@ -16,7 +18,7 @@ contract minter is Ownable {
     }
 
     function mint(uint256 tokenId) external {
-        require(nftContract.totalSupply() < MyNFT.MAX_SUPPLY, "All NFTs have been minted");
+        require(nftContract.totalSupply() < nftContract.MAX_SUPPLY(), "All NFTs have been minted");
 
         // Transfer ERC20 tokens to this contract
         require(paymentToken.transferFrom(msg.sender, address(this), PRICE), "Payment failed");
