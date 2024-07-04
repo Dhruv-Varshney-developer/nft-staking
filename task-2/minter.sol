025ca19971d5533451394984fc74650fe5cd618b
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2024-07-04 11:48:43 +0530

    [improved]: minter.sol

--- task-2/minter.sol
+++ task-2/minter.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "module-2/task-2/myERC721.sol";
+import "module-2/task-2/MyERC20.sol";
 
 contract minter is Ownable {
-    IERC20 public paymentToken;
+    MyERC20Token public paymentToken;
     tigernft public nftContract;
     uint256 public constant PRICE = 10 * 10 ** 18; // 10 tokens with 18 decimals
 
 
-    constructor(IERC20 _paymentToken, tigernft _nftContract) Ownable(msg.sender)  {
+    constructor(MyERC20Token _paymentToken, tigernft _nftContract) Ownable(msg.sender)  {
         paymentToken = _paymentToken;
         nftContract = _nftContract;
     }
