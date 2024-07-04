1816d9e532c13e2338e1e3ce22b06c2fde70f7ba
Dhruv-Varshney-developer <f20200865@pilani.bits-pilani.ac.in>
2024-07-04 11:49:43 +0530

    [improved]: minter.sol

--- task-2/minter.sol
+++ task-2/minter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "module-2/task-2/myERC721.sol";
 import "module-2/task-2/MyERC20.sol";
